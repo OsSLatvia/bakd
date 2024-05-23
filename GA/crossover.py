80a79b583b89d116b78fed787edf8f5bffb2a23e
OsSLatvia <RTL@inbox.lv>
2024-05-23 16:00:36 +0300

    GA with pygad library

--- GA/crossover.py
+++ GA/crossover.py
@@ -2,14 +2,13 @@ import random
 from GA.validate import is_valid
 from GA.repair import repair_gene
 import copy
+import numpy as np
 
 def crossover_function(parents, offspring_size, ga_instance):
     offspring = []
     num_parents = len(parents)
-    # tries=0
     while len(offspring) < offspring_size[0]:
-        # tries = tries+1
-        # print (tries)
+
         parent1_idx = random.randint(0, num_parents - 1)
         parent2_idx = random.randint(0, num_parents - 1)
 
@@ -17,8 +16,11 @@ def crossover_function(parents, offspring_size, ga_instance):
         parent2 = copy.deepcopy(parents[parent2_idx])
         
         crossover_point = random.randint(1, len(parent1) - 1)
-        offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
-        offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
+        # offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
+        # offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
+        offspring1 = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+        offspring2 = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
+
         
         # Validate and repair offspring1
         offspring1_valid = all(is_valid(offspring1, gene) for gene in offspring1) or all(repair_gene(offspring1, gene) for gene in offspring1)
--- main.py
+++ main.py
@@ -10,45 +10,79 @@ from GA.fitness import fitness_function
 import global_data
 from GA.crossover import crossover_function
 from GA.validate import is_valid_chromosome
+import pygad
+import numpy as np
+
+def fitness_func(ga_instance, solution, solution_idx):
+    return fitness_function(solution)
+def crossover_func(parents, offspring_size, ga_instance):
+        return np.array(crossover_function(parents, offspring_size, ga_instance))
+
+class CustomGA(pygad.GA):
+    def __init__(self, create_initial_population, *args, **kwargs):
+        self.create_initial_population = create_initial_population
+        super().__init__(*args, **kwargs)
+        # Initialize population as a numpy array
+        self.population = np.array(self.create_initial_population(self.sol_per_pop))
+
+    # def crossover_func(self, parents, offspring_size, ga_instance):
+    #     print("crossover works")
+
+    #     return crossover_function(parents, offspring_size, ga_instance)
+
+    def mutation(self, offspring, ga_instance):
+        print("mutation works")
+        # No mutation logic for now since mutation_probability is set to 0
+        return offspring
+
+    def cal_pop_fitness(self):
+        fitness = []
+        for i, solution in enumerate(self.population):
+            fitness.append(self.fitness_func(self, solution, i))  # Pass the instance of the GA class
+        fitness_np = np.array(fitness)  # Convert fitness list to numpy array
+        return fitness_np
+
+    # def create_population(self):
+    #     pop = self.create_initial_population(self.sol_per_pop)
+    #     print(pop)
+    #     return pop
+
+    
 all_events = global_data.all_events
 all_teachers = global_data.all_teachers
 all_classrooms = global_data.all_classrooms
 all_timeslots = global_data.all_timeslots
 student_groups = global_data.student_groups
 
-gene1= Gene(all_events[0])
-gene2= Gene(all_events[1])
-classroom1 = Classroom(13, 9)
-teacher1 = Teacher  ("Janis")
-teacher2 = Teacher  ("Karlis")
-timeslot1 = Timeslot (1)
-timeslot2 = Timeslot (2)
-gene1.set_classroom(all_classrooms[0])
-gene1.set_teacher(all_teachers[0])
-gene1.set_timeslot(timeslot1)
-gene2.set_classroom(all_classrooms[0])
-gene2.set_teacher(all_teachers[1])
-gene2.set_timeslot(timeslot2)
-chromosome = [gene1, gene2]
-print(chromosome)
-print(gene1.is_gene_timeslot_valid(chromosome))
-print(gene2.is_room_size_valid())
-print(gene2)
-print("-----> ", all_timeslots)
-print("________________")
-initial_population = create_initial_population(200)  
-# print (initial_population)
-print ("fitness:")
-print(fitness_function(initial_population[0]))
-print(fitness_function(initial_population[1]))
-print(fitness_function(initial_population[2]))
-print(fitness_function(initial_population[3]))
-offsprings = crossover_function([initial_population[0], initial_population[1]], [2], None)
-# print("_--------------------------------")
-# print (offsprings[0])
-# print("_--------------------------------")
-# print (initial_population[0])
-# print("_--------------------------------")
-print(fitness_function(offsprings[0]))
-# for pop in initial_population:
-#     print(is_valid_chromosome(pop))
+# initial_population = create_initial_population(200)  
+# print ("fitness:")
+# print(fitness_function(initial_population[0]))
+# offsprings = crossover_function([initial_population[0], initial_population[1]], [2], None)
+# print(fitness_function(offsprings[0]))
+# # PyGAD configuration
+initial_population_size = 10
+num_genes= len(all_events)
+ga_instance = CustomGA(
+    create_initial_population=create_initial_population,
+    num_generations=20,                 # Number of generations
+    num_parents_mating=10,                # Number of parents to select for mating
+    fitness_func=fitness_func,           # Fitness function
+    sol_per_pop=initial_population_size, # Population size
+    num_genes=num_genes,                 # Number of genes in each solution
+    gene_type=object,                    # Type of gene (set to object for custom class)
+    parent_selection_type='tournament',  # Parent selection type
+    crossover_probability=0.7,           # Crossover probability
+    mutation_probability=0.0,           # Set mutation probability to 0
+    crossover_type=crossover_func
+)
+
+ga_instance.run()
+
+# Get the best solution
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+
+print("Best solution found:", solution)
+print("Fitness value of the best solution:", solution_fitness)
+# print("Population:")
+# for solution in ga_instance.population:
+#     print(solution)
